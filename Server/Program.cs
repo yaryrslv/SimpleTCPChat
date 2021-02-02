37407ffc26cfbb67939ce4a939a5bc0681b984f1
Yaroslav <yarryslv@gmail.com>
2021-02-02 13:34:15 +0200

    Add main logic to Server project

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Threading;
 
 namespace Server
 {
     class Program
     {
+        static ServerCore server;
+        static Thread listenThread;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                server = new ServerCore();
+                listenThread = new Thread(new ThreadStart(server.Listen));
+                listenThread.Start();
+            }
+            catch (Exception ex)
+            {
+                server.Disconnect();
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
