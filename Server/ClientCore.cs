7f73468bfcf7d6d1ad5d9632a2e8639da27c6233
Yaroslav <yarryslv@gmail.com>
2021-02-02 12:51:52 +0200

    Add structure to ClientCore

--- Server/ClientCore.cs
+++ Server/ClientCore.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using System.Text;
 
 namespace Server
 {
     class ClientCore
     {
+        protected internal string Id { get; private set; }
+        protected internal NetworkStream Stream { get; private set; }
+        string userName;
+        TcpClient client;
+        ServerCore server;
+
+        public ClientCore(TcpClient tcpClient, ServerCore serverCore)
+        {
+            Id = Guid.NewGuid().ToString();
+            client = tcpClient;
+            server = serverCore;
+        }
+        public void Process()
+        {
+            
+        }
+        private string GetMessage()
+        {
+
+        }
+        protected internal void Close()
+        {
+
+        }
     }
 }
