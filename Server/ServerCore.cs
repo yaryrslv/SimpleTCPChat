56b3ba6f468b06a91ca14f15470808d59cfdc652
Yaroslav <yarryslv@gmail.com>
2021-02-02 12:56:53 +0200

    Add ServerCore structure

--- Server/ServerCore.cs
+++ Server/ServerCore.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 
 namespace Server
 {
     class ServerCore
     {
+        static TcpListener tcpListener;
+        List<ClientCore> clients = new List<ClientCore>();
+
+        protected internal void AddConnection(ClientCore clientObject)
+        {
+
+        }
+        protected internal void RemoveConnection(string id)
+        {
+            
+        }
+        protected internal void Listen()
+        {
+            
+        }
+        protected internal void BroadcastMessage(string message, string id)
+        {
+           
+        }
+        protected internal void Disconnect()
+        {
+            
+        }
     }
 }
