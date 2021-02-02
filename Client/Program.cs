08e3c2be175941bfefc05cbb87164171a4e03364
Yaroslav <yarryslv@gmail.com>
2021-02-02 13:36:08 +0200

    Add structure to Program class in Client project

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Net.Sockets;
+using System.Text;
 
 namespace Client
 {
     class Program
     {
+        static string userName;
+        private const string host = "127.0.0.1";
+        private const int port = 8888;
+        static TcpClient client;
+        static NetworkStream stream;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+        }
+        static void SendMessage()
+        {
+
+        }
+        static void ReceiveMessage()
+        {
+            
+        }
+        static void Disconnect()
+        {
+           
         }
     }
 }
